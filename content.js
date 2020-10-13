c29c780cc37e544f9a2a8301c1a86c175aa5f521
laport-n <nicolas-1301@live.fr>
2020-10-13 18:20:39 +0200

    Suppression fichier style, inutile

--- content.js
+++ content.js
@@ -1,8 +1,1 @@
-if (window.location.href === "https://www.google.com/"){
-  document.body.style.backgroundColor = "#FF8A65";
-  const h1 = document.createElement("h1");
-  h1.style.textAlign = "center";
-  const text = document.createTextNode("Hello World");
-  h1.appendChild(text);
-  document.getElementById("body").appendChild(h1);
-}
+console.log("Hello world");
